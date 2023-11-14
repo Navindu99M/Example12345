fd092e33e3a3a69814a62097517c3695c3712a2a
NavinduRathnayake <navindu088@gmail.com>
2023-11-14 17:33:38 +0530

    all
    gg

---
+++ programming C#/Application01/Application01/GetNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application01
+{
+    class GetNumbers
+    {
+
+        public double Number01;
+        public double Number02;
+        public void GetUserInputs()
+        {
+            Console.WriteLine("Enter Number 01 :");
+            double Number01 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Number 02 :");
+            double Number02 = double.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ programming C#/Application01/Application01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Application01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Summation objSum = new Summation();
+
+            Console.WriteLine("Summation is " + objSum.GetSummation());
+
+            Console.ReadKey();
+
+        }
+    }
+}
